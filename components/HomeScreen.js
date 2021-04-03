e7cc1fad09e0a9ec6128cf5c4d515cb2105af68b
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2021-04-02 20:52:40 -0400

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -10,51 +10,6 @@ const HomeScreen = (props) => {
        <Image source ={require ('../assets/logo.png')} style = {styles.image}> 
        </Image> 
       </ImageBackground>  
-
-      
-<<<<<<< HEAD
-      
-         
-=======
-      {/* icons documentation: https://docs.expo.io/guides/icons/ */}
-      <View style = {styles.bottomContainer}>  
-       
-        <TouchableOpacity
-          style={styles.sideContainer}
-          onPress={() => console.log("Maintenance")}
-        >
-          <Entypo name="tools" size={24} color="white" />
-          <Text style={{ color: "white" }}>Maintenance</Text>
-        </TouchableOpacity>
-     
-        <TouchableOpacity
-          style={styles.side1Container}
-          onPress={() => console.log("Contact")}
-        >
-          <Feather name="user" size={24} color="white" />
-          <Text style={{ color: "white" }}>Account</Text>
-        </TouchableOpacity> 
-     
-        <TouchableOpacity
-          style={styles.side1Container}
-          onPress={() => console.log("Contact")}
-        >
-          <Feather name="list" size={24} color="white" />
-          <Text style={{ color: "white" }}>Listings</Text>
-        </TouchableOpacity> 
- 
-
-        <TouchableOpacity
-          style={styles.side1Container}
-          onPress={() => console.log("Contact")}
-        >
-          <Feather name="phone-call" size={24} color="white" />
-          <Text style={{ color: "white" }}>Contact us</Text>
-        </TouchableOpacity>   
-       
->>>>>>> c2d9f91d0967c62814c4b63ef9c910f97fd9714a
-      
-      </View>
      
     </View>
     
@@ -80,82 +35,6 @@ const styles = StyleSheet.create({
     flexDirection: "column", 
     width: '100%', 
     height: '100%',   
-<<<<<<< HEAD
-  },  
-
-  image : {
-    alignContent: "center", 
-    justifyContent: "center",  
-    alignSelf: "center"
-=======
-  }, 
-
-  bottomContainer: {  
-    flexDirection: "row",
-    flexWrap: "wrap", 
-    backgroundColor: "#8DA9C4",
-     
-  },  
-
-  bottom1Container: { 
-    flex: 1,
-    flexDirection: "row", 
-    backgroundColor: "#8DA9C4",
-    
-  }, 
-
-  sideContainer: {  
-    alignItems: "center",
-    borderWidth: 1,
-    //borderRadius: 4,
-    borderColor: "lightgray",
-    backgroundColor: "#8DA9C4",
-    padding: 10,
-    width: "50%",
-    //marginRight: 10,
-    //marginTop: 10, 
-   
-  }, 
-
-  side1Container: {  
-    alignItems: "center",
-    borderWidth: 1,
-    //borderRadius: 4,
-    borderColor: "lightgray",
-    backgroundColor: "#8DA9C4",
-    padding: 10,
-    width: "50%",
-    //marginRight: 10,
-    //marginTop: 10,
-   
-  }, 
-
-  side2Container : {
-    alignItems: "center",
-    borderWidth: 1,
-    //borderRadius: 4,
-    borderColor: "lightgray",
-    backgroundColor: "#8DA9C4",
-    padding: 10,
-    width: "50%",
-    //marginRight: 10,
-    //marginTop: 10,
-   
-  }, 
-
-  side3Container: {
-    alignItems: "center",
-    borderWidth: 1,
-    //borderRadius: 4,
-    borderColor: "lightgray",
-    backgroundColor: "#8DA9C4",
-    padding: 10,
-    width: "50%",
-    //marginRight: 10,
-    //marginTop: 10, 
->>>>>>> c2d9f91d0967c62814c4b63ef9c910f97fd9714a
-  }
-  
     
  
   
