d8adf935b99feab54652bafea7e2fabc4859aa89
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2021-04-04 03:09:34 -0400

    Update HomeScreen.js
    Where the logo is, will be replaced be slider box of pictures. Where text is will be the 'about' section plan to make it smaller

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
+import { Component } from "react";
 import { Dimensions, ImageBackground } from "react-native";
-import {Image, ScrollView, StyleSheet,  Text, TouchableOpacity,  View } from "react-native"; 
+import {Image, ScrollView, StyleSheet, Slider,  Text, TouchableOpacity,  View } from "react-native"; 
 
 const {height, width} = Dimensions.get("window"); 
 
-const HomeScreen = (props) => {
+class HomeScreen extends Component { 
+  render () {
   return (
     <View style = {styles.container}>
-      <View style ={styles.slider}>
+      <View style ={styles.slider}> 
+
+      <Image source={require("../assets/logo.png")} />
        
       </View> 
-      <View style = {styles.footer}> 
+      <View style = {styles.footer}>  
+      <Text style ={styles.text}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </Text>
       </View> 
-      <View style = {styles.footer1}> 
-      </View>
+    
     </View> 
 
 
 
   );
-
+  }
   };
 
 const styles = StyleSheet.create({
@@ -31,15 +36,20 @@ const styles = StyleSheet.create({
   
   slider: {
     flex: 2 * height, 
-    backgroundColor: "#0B2545",  
+    backgroundColor: "#8DA9C4",  
     borderTopRightRadius: 75, 
     borderBottomLeftRadius: 75
    
   }, 
 
   footer: {
-    flex: 1 * height 
-  } 
+    flex: 1 * height
+
+  }, 
+  
+  text: {
+   color: "#0B2545", 
+  }
 
   
 
