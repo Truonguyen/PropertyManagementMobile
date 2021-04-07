03ab53294b122a22141914f07826fbb007f09a79
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2021-04-06 21:53:13 -0400

    Update HomeScreen.js

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -24,7 +24,7 @@ const HomeScreen = (props) => {
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       
 
-      </View> 
+      </View>  
       
         
     </View> 
