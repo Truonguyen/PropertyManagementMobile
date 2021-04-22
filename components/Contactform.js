82f7ec018bf09c526126bb7c0a0066220eb5b0bb
Jess0420 <53028568+Jess0420@users.noreply.github.com>
2021-04-22 03:49:25 -0400

    Create Contactform.js

---
+++ components/Contactform.js
@@ -0,0 +1,76 @@
+import React from "react"; 
+import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { Formik } from "formik";  
+
+const ContactForm = (props) => {
+    return ( 
+
+        <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ title: "", body: "", rating: "" }}
+        // handleSubmit run onSubmit
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review title"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+            />
+
+            <TextInput
+              style={globalStyles.input}
+              multiline
+              placeholder="Review details"
+              onChangeText={props.handleChange("body")}
+              value={props.values.body}
+            />
+
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1 - 5)"
+              onChangeText={props.handleChange("rating")}
+              value={props.values.rating}
+              keyboardType="numeric"
+            />
+
+            <Button
+              color="maroon"
+              title="Submit"
+              onPress={props.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+export default ContactForm;
+
+export const globalStyles = StyleSheet.create({
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
+  },
+});   
